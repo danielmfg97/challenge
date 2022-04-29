dbb2314d037d7f261454a4caa80b9b6f2607509b
Daniel <danielmfg97@gmail.com>
2022-04-29 20:48:30 -0300

    Remove DTO

--- src/decompose-number/dto/update-decompose-number.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDecomposeNumberDto } from './create-decompose-number.dto';
-
-export class UpdateDecomposeNumberDto extends PartialType(CreateDecomposeNumberDto) {}
