3e86e5c563caaf8058208507d9514370095a7f3f
Daniel <danielmfg97@gmail.com>
2022-04-29 20:49:21 -0300

    Remove unused routes

--- src/decompose-number/decompose-number.service.ts
+++ src/decompose-number/decompose-number.service.ts
@@ -1,19 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { CreateDecomposeNumberDto } from './dto/create-decompose-number.dto';
-import { UpdateDecomposeNumberDto } from './dto/update-decompose-number.dto';
 import { decomposeNumber } from '../utils/decompose-number'
 import { checkPrime } from 'src/utils/prime-numbers';
 
 @Injectable()
 export class DecomposeNumberService {
-  create(createDecomposeNumberDto: CreateDecomposeNumberDto) {
-    return 'This action adds a new decomposeNumber';
-  }
-
-  findAll() {
-    return `This action returns all decomposeNumber`;
-  }
-
   async findOne(id: number) {
     const decomposed = await decomposeNumber(id);
 
@@ -29,13 +19,5 @@ export class DecomposeNumberService {
     }
 
     return {decomposed, primeNumbers}
-  }
-
-  update(id: number, updateDecomposeNumberDto: UpdateDecomposeNumberDto) {
-    return `This action updates a #${id} decomposeNumber`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} decomposeNumber`;
   }
 }
